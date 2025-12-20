0c2c65d36c902fb6f5ede1f8a6cf3e20a40af57d
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-19 16:01:10 -0800

    Player Script Update

--- minijam-200/Scripts/PlayerMovement.gd
+++ minijam-200/Scripts/PlayerMovement.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 #Who's Rem
+var DefSpeed = 20
 var Speed = 20
+var Aiming = false
 
 func _physics_process(delta: float) -> void:
+	if(Aiming == true):
+		Speed = 5
+	else:
+		Speed = DefSpeed
+		
 	var InputDir = Input.get_vector("Left","Right", "Up", "Down")
 	velocity += InputDir * Speed
 	velocity*=0.9
+	
+	
 	
 	move_and_slide()

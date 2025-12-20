35dee9e807eff10c3010358945f170f708dff07f
arrowira <41136800+arrowira@users.noreply.github.com>
2025-12-19 21:31:01 -0800

    frogs in the main scene

--- minijam-200/Scripts/frog.gd
+++ minijam-200/Scripts/frog.gd
@@ -1,26 +1,24 @@
 extends RigidBody2D
 
 var jumpDir = Vector2.RIGHT
-var jumpDist = 5
+var jumpDist = 3
 var jumping = false
 var jumpStartPos = Vector2.ZERO
 var t = 0
-var height = 0.3
+var height = 0.2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Space"):
-		jump(jumpDir)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float):
 	if jumping:
 		t+=1
-		print(t)
 		position += jumpDir*jumpDist
 		$Sprite2D.position.y = height*(t-60)*t
 		
 func jump(dir):
+	jumpDir = dir
 	t = 0
 	jumping=true
 	$jumpTimer.start()
@@ -31,3 +29,12 @@ func _on_jump_timer_timeout() -> void:
 	print(t)
 	jumping=false
 	$CollisionShape2D.visible = true
+
+
+func _on_jump_cooldown_timeout() -> void:
+	var movementChoice = randi_range(0,10)
+	if movementChoice < 4:
+		var randX = randf_range(-1,1)
+		var randY = randf_range(-1,1)
+		var newDir = Vector2(randX,randY).normalized()
+		jump(newDir)

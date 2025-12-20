ece67cdad72e132fb9769fe4597d26c0c5b258d7
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-19 17:11:30 -0800

    Maybe Fix

--- minijam-200/Scripts/PlayerMovement.gd
+++ minijam-200/Scripts/PlayerMovement.gd
@@ -6,6 +6,10 @@ var Aiming = false
 var BowCharge = 0
 var ArrowObj = preload("res://Scenes/Arrow.tscn")
 
+var Dashing = false
+var DashTime = 0
+var DashSpeed = 7
+
 func _physics_process(delta: float) -> void:
 	if(BowCharge >= 25 && !Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)):
 		BowCharge = 0
@@ -25,6 +29,23 @@ func _physics_process(delta: float) -> void:
 		
 	var InputDir = Input.get_vector("Left","Right", "Up", "Down")
 	velocity += InputDir * Speed
-	velocity*=0.9
+	if(DashTime > 7 || Dashing == false):
+		velocity*=0.9
+	
+	if(Input.is_action_just_pressed("Dash") && Dashing == false):
+		Dashing = true
+	
+	if(Dashing == true):
+		if(DashTime == 0):
+			velocity*=DashSpeed
+		DashTime+=1
+	if(DashTime == 40):
+		DashTime = 0
+		Dashing = false
+		
+	if(Dashing == true && DashTime <= 15):
+		$Sprite.self_modulate.a = 0.5
+	else:
+		$Sprite.self_modulate.a = 1
 	
 	move_and_slide()

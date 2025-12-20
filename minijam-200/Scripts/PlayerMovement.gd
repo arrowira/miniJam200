af966aff24cddf234da06d7522fe974ab5d05561
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-19 16:38:01 -0800

    Arrow Shooting

--- minijam-200/Scripts/PlayerMovement.gd
+++ minijam-200/Scripts/PlayerMovement.gd
@@ -3,9 +3,22 @@ extends CharacterBody2D
 var DefSpeed = 20
 var Speed = 20
 var Aiming = false
+var BowCharge = 0
+var ArrowObj = preload("res://Scenes/Arrow.tscn")
 
 func _physics_process(delta: float) -> void:
-	if(Aiming == true):
+	if(BowCharge >= 25 && !Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)):
+		BowCharge = 0
+		var Arrow = ArrowObj.instantiate()
+		Arrow.look_at(get_global_mouse_position())
+		add_child(Arrow)
+		
+	if(Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)):
+		BowCharge+=1
+	else:
+		BowCharge = 0
+		
+	if(BowCharge > 0):
 		Speed = 5
 	else:
 		Speed = DefSpeed
@@ -13,7 +26,5 @@ func _physics_process(delta: float) -> void:
 	var InputDir = Input.get_vector("Left","Right", "Up", "Down")
 	velocity += InputDir * Speed
 	velocity*=0.9
-	
-	
 	
 	move_and_slide()

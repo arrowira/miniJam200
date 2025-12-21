c8179ef93f1123da012e6e95b41096fef7a2d563
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-20 18:52:21 -0800

    Gore

---
+++ minijam-200/Scenes/GoreController.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+var Speed = 0.05
+var DriftPos = 0
+var RotSpeed = 0.01
+var T = 0
+
+func _ready() -> void:
+	frame = randi_range(-1, 3)
+	DriftPos = global_position + Vector2(randi_range(-100, 100), randi_range(-100, 100))
+	
+func _physics_process(delta: float) -> void:
+	var TDir = (DriftPos - global_position).normalized()
+	position+=TDir * Speed
+	
+	var TAngle = (DriftPos - global_position).angle()
+	rotation = lerp_angle(rotation, TAngle, RotSpeed)
+	
+	T+=1
+	if(T>=50):
+		modulate.a = 1-((T-49)/50)
+	if(T >= 100):
+		queue_free()
+	
--- minijam-200/Scripts/Arrow.gd
+++ minijam-200/Scripts/Arrow.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var FrogGoreObj = preload("res://Scenes/FrogGoreTemplate.tscn") 
+
 var DespawnTimer = 0
 var GreenParts = preload("res://Scenes/green_frog_death_particles.tscn")
 
@@ -12,6 +14,15 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.name == "frogArea2D":
 		$DeathSound.play()
 		area.get_parent().queue_free()
+		SpawnFrogGore()
 		var parts = GreenParts.instantiate()
 		parts.global_position = global_position
 		get_parent().get_parent().add_child(parts)
+		
+		
+func SpawnFrogGore():
+	for i in range(randi_range(3, 5)):
+		var S = FrogGoreObj.instantiate()
+		S.position = global_position + Vector2(randi_range(-20, 20), randi_range(-20, 20))
+		S.rotation = randf() * TAU
+		get_tree().root.add_child(S)

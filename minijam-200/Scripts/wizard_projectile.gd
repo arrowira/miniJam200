8e821386a7de96ade63cf14cc89479a3a1a44b54
arrowira <41136800+arrowira@users.noreply.github.com>
2025-12-21 19:51:03 -0800

    wizard projectiles do damage

--- minijam-200/Scripts/wizard_projectile.gd
+++ minijam-200/Scripts/wizard_projectile.gd
@@ -1,11 +1,24 @@
 extends RigidBody2D
 
 @export var speed := 80.0
+var lifetime = 300
+var life = 0
 var player_dir := Vector2.ZERO
 
 func _ready() -> void:
 	$projArea/AnimationPlayer.play("spawn")
-
+func _physics_process(delta: float) -> void:
+	life+=1
+	if life == lifetime:
+		$projArea/AnimationPlayer.play("die")
+	if life == lifetime+20:
+		queue_free()
 func launch(from_pos: Vector2, target_pos: Vector2) -> void:
 	player_dir = (target_pos - from_pos).normalized()
 	linear_velocity = player_dir * speed
+
+
+func _on_proj_area_area_entered(area: Area2D) -> void:
+	if area.name == "playerArea":
+		area.get_parent().Damage(1)
+		queue_free()

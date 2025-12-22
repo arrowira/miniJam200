0271b408b74c3823430aa4d3335c72d2323a39aa
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-22 03:13:20 -0800

    UpgradeUI Infastructure

--- minijam-200/Scripts/gameManager.gd
+++ minijam-200/Scripts/gameManager.gd
@@ -1,16 +1,28 @@
 extends Node2D
 
+var UpgradeArray
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	UpgradeArray = []
+	for i in range(6):
+		UpgradeArray.append(0)
+	Upgraded()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+# Run when anything is upgraded to update the disaplay 
+func Upgraded():
+	if(UpgradeArray[0]!= 0):
+		$CanvasLayer/UpgradeUI/HealthUpgrade/Progress.scale = Vector2(0.4375/(8-UpgradeArray[0]), $CanvasLayer/UpgradeUI/HealthUpgrade/Progress.scale.y)
+	if(UpgradeArray[1]!= 0):
+		$CanvasLayer/UpgradeUI/ArrowPenetration/Progress.scale = Vector2(0.4375/(8-UpgradeArray[1]), $CanvasLayer/UpgradeUI/ArrowPenetration/Progress.scale.y)
+	if(UpgradeArray[2]!= 0):
+		$CanvasLayer/UpgradeUI/MoreArrows/Progress.scale = Vector2(0.4375/(8-UpgradeArray[2]), $CanvasLayer/UpgradeUI/MoreArrows/Progress.scale.y)
+	if(UpgradeArray[3]!= 0):
+		$CanvasLayer/UpgradeUI/Speed/Progress.scale = Vector2(0.4375/(8-UpgradeArray[3]), $CanvasLayer/UpgradeUI/Speed/Progress.scale.y)
+	if(UpgradeArray[4]!= 0):
+		$CanvasLayer/UpgradeUI/DashDistance/Progress.scale = Vector2(0.4375/(8-UpgradeArray[4]), $CanvasLayer/UpgradeUI/DashDistance/Progress.scale.y)
+	if(UpgradeArray[5]!= 0):
+		$CanvasLayer/UpgradeUI/AimSpeed/Progress.scale = Vector2(0.4375/(8-UpgradeArray[5]), $CanvasLayer/UpgradeUI/AimSpeed/Progress.scale.y)
+	
 func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()
 	

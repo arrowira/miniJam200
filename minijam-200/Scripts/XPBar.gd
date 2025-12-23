a7f192b512fcbdd8007dbf5e815c65e6b199c1e2
arrowira <41136800+arrowira@users.noreply.github.com>
2025-12-22 19:18:01 -0800

    xp bar behaviour

---
+++ minijam-200/Scripts/XPBar.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var xp=0
+var lvl = 0
+
+var lvlLengthMod = 1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("space"):
+		addXP(10)
+
+func addXP(amt):
+	xp+=int(amt*lvlLengthMod)
+	if xp>=100:
+		xp=xp%100
+		lvl += 1
+		$lvlLabel.text = str(lvl)
+		lvlLengthMod *= 0.9
+	
+	
+	$ProgressBar.value=xp

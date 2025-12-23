257a3bad784e6cd6a0537315d7f8679d0fefa19a
arrowira <41136800+arrowira@users.noreply.github.com>
2025-12-23 15:37:17 -0800

    main menu transitions

---
+++ minijam-200/Scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_settings_pressed() -> void:
+	$camAnimations.play("toSettings")
+
+
+func _on_back_pressed() -> void:
+	$camAnimations.play("toMain")

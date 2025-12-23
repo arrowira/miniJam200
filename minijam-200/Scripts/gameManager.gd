7b675f70a26bfa179e566112bbba7498210c0204
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-22 17:07:36 -0800

    Progress Bar Fix

--- minijam-200/Scripts/gameManager.gd
+++ minijam-200/Scripts/gameManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var UpgradeArray
+var BarSize = 7.77
 
 func _ready() -> void:
 	UpgradeArray = []
@@ -44,17 +45,17 @@ func _physics_process(delta: float) -> void:
 # Run when anything is upgraded to update the disaplay 
 func Upgraded():
 	if(UpgradeArray[0]!= 0):
-		$CanvasLayer/UpgradeUI/HealthUpgrade/Progress.scale = Vector2((0.4375/8)*(UpgradeArray[0]), $CanvasLayer/UpgradeUI/HealthUpgrade/Progress.scale.y)
+		$CanvasLayer/UpgradeUI/HealthUpgrade/Progress.scale = Vector2((BarSize/8)*(UpgradeArray[0]), $CanvasLayer/UpgradeUI/HealthUpgrade/Progress.scale.y)
 	if(UpgradeArray[1]!= 0):
-		$CanvasLayer/UpgradeUI/ArrowPenetration/Progress.scale = Vector2((0.4375/8)*(UpgradeArray[1]), $CanvasLayer/UpgradeUI/ArrowPenetration/Progress.scale.y)
+		$CanvasLayer/UpgradeUI/ArrowPenetration/Progress.scale = Vector2((BarSize/8)*(UpgradeArray[1]), $CanvasLayer/UpgradeUI/ArrowPenetration/Progress.scale.y)
 	if(UpgradeArray[2]!= 0):
-		$CanvasLayer/UpgradeUI/MoreArrows/Progress.scale = Vector2((0.4375/8)*(UpgradeArray[2]), $CanvasLayer/UpgradeUI/MoreArrows/Progress.scale.y)
+		$CanvasLayer/UpgradeUI/MoreArrows/Progress.scale = Vector2((BarSize/8)*(UpgradeArray[2]), $CanvasLayer/UpgradeUI/MoreArrows/Progress.scale.y)
 	if(UpgradeArray[3]!= 0):
-		$CanvasLayer/UpgradeUI/Speed/Progress.scale = Vector2((0.4375/8)*(UpgradeArray[3]), $CanvasLayer/UpgradeUI/Speed/Progress.scale.y)
+		$CanvasLayer/UpgradeUI/Speed/Progress.scale = Vector2((BarSize/8)*(UpgradeArray[3]), $CanvasLayer/UpgradeUI/Speed/Progress.scale.y)
 	if(UpgradeArray[4]!= 0):
-		$CanvasLayer/UpgradeUI/DashDistance/Progress.scale = Vector2((0.4375/8)*(UpgradeArray[4]), $CanvasLayer/UpgradeUI/DashDistance/Progress.scale.y)
+		$CanvasLayer/UpgradeUI/DashDistance/Progress.scale = Vector2((BarSize/8)*(UpgradeArray[4]), $CanvasLayer/UpgradeUI/DashDistance/Progress.scale.y)
 	if(UpgradeArray[5]!= 0):
-		$CanvasLayer/UpgradeUI/AimSpeed/Progress.scale = Vector2((0.4375/8)*(UpgradeArray[5]), $CanvasLayer/UpgradeUI/AimSpeed/Progress.scale.y)
+		$CanvasLayer/UpgradeUI/AimSpeed/Progress.scale = Vector2((BarSize/8)*(UpgradeArray[5]), $CanvasLayer/UpgradeUI/AimSpeed/Progress.scale.y)
 	
 func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()

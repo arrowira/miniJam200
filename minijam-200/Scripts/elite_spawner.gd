a8022a08433d48eb47e083074a17d476de5881de
arrowira <41136800+arrowira@users.noreply.github.com>
2025-12-22 22:09:00 -0800

    elite spawning

---
+++ minijam-200/Scripts/elite_spawner.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var eliteFrog = preload("res://Scenes/eliteFrog.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_spawn_timer_timeout() -> void:
+	var spawnChance = randi_range(0,15)
+	if spawnChance == 5:
+		var newFrog = eliteFrog.instantiate()
+		get_tree().root.get_node("main").add_child(newFrog)
+		newFrog.global_position = global_position

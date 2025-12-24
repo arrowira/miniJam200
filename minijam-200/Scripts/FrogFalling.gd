39d401b9f744c65422479dab33e30b9c8c7efbb0
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-12-23 16:28:43 -0800

    Falling Objcs

---
+++ minijam-200/Scripts/FrogFalling.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var FrogGoreObj = preload("res://Scenes/FrogGoreTemplateMenu.tscn")  
+
+func _physics_process(delta: float) -> void:
+	if(randi_range(1, 5) == 1):
+		var type = randi_range(0, 3)
+		var S = FrogGoreObj.instantiate()
+		if(randi_range(0, 3) == 1):
+			S.get_node("Gore").texture = preload("res://Sprites/WizardGore.png")
+		if(randi_range(0, 3) == 1):
+			S.get_node("Gore").texture = preload("res://Sprites/HealthFrogGore.png")
+		if(randi_range(0, 3) == 1):
+			S.get_node("Gore").texture = preload("res://Sprites/EliteFrogGore.png")
+		if(randi_range(0, 5) == 1):
+			S.get_node("Gore").texture = preload("res://Sprites/Health.png")
+			S.get_node("Gore").hframes = 1
+			S.get_node("Gore").vframes = 2
+			S.get_node("Gore").scale = Vector2(0.07, 0.07)
+		if(randi_range(0, 10) == 1):
+			S.get_node("Gore").texture = preload("res://Sprites/arrow-1.png.png")
+			S.get_node("Gore").hframes = 1
+			S.get_node("Gore").vframes = 1
+			S.get_node("Gore").scale = Vector2(0.04, 0.04)
+		if(randi_range(0, 50) == 1):
+			S.get_node("Gore").texture = preload("res://Sprites/eliteFrog (1).png")
+			S.get_node("Gore").hframes = 3
+			S.get_node("Gore").vframes = 2
+			S.get_node("Gore").scale = Vector2(1, 1)
+		S.get_node("Gore").MenuType = true
+		S.position +=Vector2(randi_range(-400, 400), -300)
+		add_child(S)
+		
+		
--- minijam-200/Scripts/GoreController.gd
+++ minijam-200/Scripts/GoreController.gd
@@ -4,20 +4,25 @@ var Speed = 0.05
 var DriftPos = 0
 var RotSpeed = 0.01
 var T = 0
+var MenuType = false
 
 func _ready() -> void:
 	frame = randi_range(0, 3)
-	DriftPos = global_position + Vector2(randi_range(-100, 100), randi_range(-100, 100))
-	
+	if(MenuType != true):
+		DriftPos = global_position + Vector2(randi_range(-100, 100), randi_range(-100, 100))
+	else:
+		get_parent().rotation = randf() * TAU  
+		get_parent().angular_velocity = randf_range(-5.0, 5.0)
 func _physics_process(delta: float) -> void:
-	var TDir = (DriftPos - global_position).normalized()
-	position+=TDir * Speed
-	
-	var TAngle = (DriftPos - global_position).angle()
-	rotation = lerp_angle(rotation, TAngle, RotSpeed)
-	
-	T+=1
-	self_modulate = Color(self_modulate.r, self_modulate.g, self_modulate.b, clamp(1.0 - T / 150.0, 0.0, 1.0))
-	if(T >= 150):
-		queue_free()
+	if(MenuType != true):
+		var TDir = (DriftPos - global_position).normalized()
+		position+=TDir * Speed
+		
+		var TAngle = (DriftPos - global_position).angle()
+		rotation = lerp_angle(rotation, TAngle, RotSpeed)
+		
+		T+=1
+		self_modulate = Color(self_modulate.r, self_modulate.g, self_modulate.b, clamp(1.0 - T / 150.0, 0.0, 1.0))
+		if(T >= 150):
+			queue_free()
 	
